140319a17505b6b92568674b0059789245d8c25e
ricardospinoza <ricardospinoza@gmail.com>
2022-09-09 17:50:08 -0300

    Implementação Stategy pattern class TesteImposto Implementação Chain of Responsibility TesteDesconto

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # design-patterns-java
-Example of design patterns java
+Example of design patterns java by Alura
+
---
+++ design-patterns-java/src/br/com/alura/loja/TesteImposto.java
@@ -0,0 +1,19 @@
+package br.com.alura.loja;
+
+import java.math.BigDecimal;
+
+import br.com.alura.loja.imposto.CalculadoraDeImpostos;
+import br.com.alura.loja.imposto.ICMS;
+import br.com.alura.loja.imposto.ISS;
+
+public class TesteImposto {
+
+	public static void main(String[] args) {
+		Orcamento orcamento = new Orcamento(new BigDecimal("100"), 1);
+		
+		CalculadoraDeImpostos calculo = new CalculadoraDeImpostos();
+		System.out.println(calculo.calcular(orcamento,new ICMS()));		
+		System.out.println(calculo.calcular(orcamento,new ISS()));		
+	}
+
+}
