789c327f0272af978725ff3c3749d22b1d842f23
ricardospinoza <ricardospinoza@gmail.com>
2022-11-09 18:39:55 -0300

    Atualizações aulas de padrão de projeto

--- design-patterns-java/src/br/com/alura/loja/TesteImposto.java
+++ design-patterns-java/src/br/com/alura/loja/TesteImposto.java
@@ -5,6 +5,7 @@ import java.math.BigDecimal;
 import br.com.alura.loja.imposto.CalculadoraDeImpostos;
 import br.com.alura.loja.imposto.ICMS;
 import br.com.alura.loja.imposto.ISS;
+import br.com.alura.loja.orcamento.Orcamento;
 
 public class TesteImposto {
 
